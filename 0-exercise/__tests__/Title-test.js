afe4052da446a3d51dab99272bc2623901d42e70
杜娟 <jdu.xian@gmail.com>
2019-05-14 13:04:09 +0800

    rename package

--- 0-exercise/__tests__/Title-test.js
+++ 0-exercise/__tests__/Title-test.js
@@ -8,5 +8,6 @@ afterEach(cleanup);
 it('Title组件渲染内容为Hello World', () => {
   const { getByTestId } = render(<Title />);
 
+  // TODO 0: 这里只是给学生演示一个最简单的React组件测试
   expect(getByTestId('title')).toHaveTextContent(/^Hello World$/);
 });
